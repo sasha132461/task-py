b9f9c6fbf63b3e35c2a52310a9f5c3c6000652a9
sasha132461 <sasha1324612@gmail.com>
2023-08-07 22:28:19 +0300

    ))

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ if result == 2:
 elif result == 1: 
     print("da")
 else:
-    for x in range(2, result, result - 1):
-        for y in range(2,x):
-            if y / x == 0:
-                y += 1
-    for i in range(2,result):
-        for j in range(2, i):
-            if i % j == 0:
-                y += 2
-if y == 1:
-    print("Da")
-else:
-    print("No")
+    for i in range(2, int(result**0.5)+1):
+        if result % i == 0:
+            print("No")
+            break
+        else:
+            print("Da")
+            break
