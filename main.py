5885a265cd50afe93f1edd041bd80ce05cd1a6dc
sasha132461 <sasha1324612@gmail.com>
2023-08-07 20:16:55 +0300

    ))

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 result = int(input())
+y = 0
 if result == 2:
     print("da")
 elif result == 1: 
     print("da")
 else:
+    for x in range(2, result, result - 1):
+        for y in range(2,x):
+            if y / x == 0:
+                y += 1
     for i in range(2,result):
-        if result % i == 0:
-            print("No")
-        elif result % 3 == 0:
-            print("No")
-        elif result % 5 == 0:
-            print("No")
-        elif result % 2 == 0:
-            print("No")
-            break
-        elif result % result == 0:
-            print("Da")
-            break
+        for j in range(2, i):
+            if i % j == 0:
+                y += 2
+if y == 1:
+    print("Da")
+else:
+    print("No")
